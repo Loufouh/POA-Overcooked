e0d24235104e5d56c8d2e9323abf2ddd48d9c709
Louis Masson <louis.masson.contact@gmail.com>
2022-10-16 23:58:57 +0200

    :ambulance: Fix test agent_rules_Condition_namespace.Condition

--- test/agent/rules/Condition/namespace/test.cpp
+++ test/agent/rules/Condition/namespace/test.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include "agent/rules/Condition/Condition.h"
 
-TEST(agent_rules_Condition_namespace, Condition) {
-    agent::rules::Condition condition;
-}
+class TestCondition : public agent::rules::Condition {};
+
+TEST(agent_rules_Condition_namespace, Condition) {}
