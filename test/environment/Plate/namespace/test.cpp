cd5aa858d1b7109c7d2879bac21cc1155ddaa47c
Louis700 <louis.masson.contact@gmail.com>
2022-10-16 14:35:32 +0200

    :white_check_mark: Add tests verifying namespace of environment::Plate

---
+++ test/environment/Plate/namespace/test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "environment/Plate.h"
+
+TEST(Plate_namespace, Plate) {
+    environment::Plate plate;
+}
+
+TEST(Plate_namespace, PlateState) {
+    environment::PlateState state;
+}
