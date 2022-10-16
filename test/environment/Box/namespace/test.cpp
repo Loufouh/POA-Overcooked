4e872db26b35832d6d6722fc35b65e0230bcf9b7
Louis700 <louis.masson.contact@gmail.com>
2022-10-16 14:31:18 +0200

    :white_check_mark: Add tests verifying namespace of environment::Box

---
+++ test/environment/Box/namespace/test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "environment/Box.h"
+
+TEST(Box_namespace, Box) {
+    environment::Box box;
+}
+
+TEST(Box_namespace, BoxState) {
+    environment::BoxState state;
+}
