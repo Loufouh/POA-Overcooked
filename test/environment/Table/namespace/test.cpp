19e7bceedcee52a06b58dee09f58c8c4744701fe
Louis700 <louis.masson.contact@gmail.com>
2022-10-16 14:33:09 +0200

    :white_check_mark: Add tests verifying namespace of environment::Table

---
+++ test/environment/Table/namespace/test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "environment/Table.h"
+
+TEST(Table_namespace, Table) {
+    environment::Table table;
+}
+
+TEST(Table_namespace, TableState) {
+    environment::TableState state;
+}
