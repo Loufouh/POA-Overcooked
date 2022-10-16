74755d82c98f6135294acee9eaf8ebbaba99bc37
Louis700 <louis.masson.contact@gmail.com>
2022-10-16 14:29:41 +0200

    :white_check_mark: Add tests verifying namespace of environment::Machine

---
+++ test/environment/Machine/namespace/test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "environment/Machine.h"
+
+TEST(Machine_namespace, Machine) {
+    environment::Machine machine;
+}
+
+TEST(Machine_namespace, MachineState) {
+    environment::MachineState state;
+}
